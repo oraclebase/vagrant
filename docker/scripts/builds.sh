8fdb73dadfb63f4c94dfa54c943991dc14437130
Tim Hall <tim@oracle-base.com>
2018-06-02 21:22:15 +0100

    Initial commit.

---
+++ docker/scripts/builds.sh
@@ -0,0 +1,33 @@
+# This is an optional file used for my setup.
+
+# Set up environment for demos.
+echo "export JAVA_HOME=/home/docker_user/java/latest" >> ~/.bash_profile
+echo "alias sql=\"/home/docker_user/sqlcl/bin/sql\"" >> ~/.bash_profile
+
+cd ~
+unzip /vagrant/software/sqlcl-18.1.1.zip
+mkdir ~/java
+cd ~/java
+tar -xvf /vagrant/software/jdk-10.0.1_linux-x64_bin.tar.gz
+ln -s ./j* ./latest
+cd ~
+unzip /vagrant/software/autorest_demo.zip
+
+# Copy ORDS software and do build.
+cd ~/dockerfiles/ords/ol7_ords/software
+cp /vagrant/software/apex_18.1_en.zip .
+cp /vagrant/software/apache-tomcat-9.0.8.tar.gz .
+cp /vagrant/software/ords.18.1.1.95.1251.zip .
+cp /vagrant/software/sqlcl-18.1.1.zip .
+cp /vagrant/software/jdk-10.0.1_linux-x64_bin.tar.gz .
+cd ..
+docker build -t ol7_ords:latest .
+
+# Copy database software and do build.
+cd ~/dockerfiles/database/ol7_122/software
+cp /vagrant/software/linuxx64_12201_database.zip .
+cp /vagrant/software/apex_18.1_en.zip .
+cd ..
+docker build -t ol7_122:latest .
+
+
