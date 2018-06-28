44a6f2bb6c94fa122f685afd18bbd409b1a46c49
Tim Hall <tim@oracle-base.com>
2018-06-28 23:07:03 +0100

    Initial commit.

---
+++ f28_122/scripts/oracle_software_installation.sh
@@ -0,0 +1,24 @@
+# Unzip software
+cd ${SOFTWARE_DIR}
+unzip -o ${DB_SOFTWARE}
+
+# Do software-only installation
+${SOFTWARE_DIR}/database/runInstaller -ignoreSysPrereqs -ignorePrereq          \
+    -waitforcompletion -showProgress -silent                                   \
+    -responseFile $SOFTWARE_DIR/database/response/db_install.rsp               \
+    oracle.install.option=INSTALL_DB_SWONLY                                    \
+    ORACLE_HOSTNAME=${ORACLE_HOSTNAME}                                         \
+    UNIX_GROUP_NAME=oinstall                                                   \
+    INVENTORY_LOCATION=${ORA_INVENTORY}                                        \
+    SELECTED_LANGUAGES=en,en_GB                                                \
+    ORACLE_HOME=${ORACLE_HOME}                                                 \
+    ORACLE_BASE=${ORACLE_BASE}                                                 \
+    oracle.install.db.InstallEdition=EE                                        \
+    oracle.install.db.OSDBA_GROUP=dba                                          \
+    oracle.install.db.OSBACKUPDBA_GROUP=dba                                    \
+    oracle.install.db.OSDGDBA_GROUP=dba                                        \
+    oracle.install.db.OSKMDBA_GROUP=dba                                        \
+    oracle.install.db.OSRACDBA_GROUP=dba                                       \
+    SECURITY_UPDATES_VIA_MYORACLESUPPORT=false                                 \
+    DECLINE_SECURITY_UPDATES=true
+
