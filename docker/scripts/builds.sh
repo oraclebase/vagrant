c5dd0d91a3f0b6ed1bdb4407d2ef50a4cf6d9200
Tim Hall <tim@oracle-base.com>
2018-07-25 22:26:06 +0100

    Add --squash to save space.
    Use full path before build.

--- docker/scripts/builds.sh
+++ docker/scripts/builds.sh
@@ -20,15 +20,15 @@ cp /vagrant/software/apache-tomcat-9.0.10.tar.gz .
 cp /vagrant/software/ords-18.2.0.zip .
 cp /vagrant/software/sqlcl-18.2.0.zip .
 cp /vagrant/software/jdk-10.0.2_linux-x64_bin.tar.gz .
-cd ..
-docker build -t ol7_ords:latest .
+cd ~/dockerfiles/ords/ol7_ords
+docker build --squash -t ol7_ords:latest .
 
 # Copy database software and do build.
 cd ~/dockerfiles/database/ol7_183/software
 cp /vagrant/software/LINUX.X64_180000_db_home.zip .
 cp /vagrant/software/apex_18.1_en.zip .
-cd ..
-docker build -t ol7_183:latest .
+cd ~/dockerfiles/database/ol7_183
+docker build --squash -t ol7_183:latest .
 
 # Start application.
 
