1e3db32dbb61357ac50566de81d5365eddf16dd5
Tim Hall <tim@oracle-base.com>
2018-08-11 18:42:24 +0100

    Change comments to echos, so you can see better what is happeing during the build.

--- docker/scripts/root_setup.sh
+++ docker/scripts/root_setup.sh
@@ -1,4 +1,6 @@
-# Prepare yum with the latest repos.
+echo "******************************************************************************"
+echo "Prepare Yum with the latest repos." `date`
+echo "******************************************************************************"
 cd /etc/yum.repos.d
 rm -f public-yum-ol7.repo
 wget http://yum.oracle.com/public-yum-ol7.repo
@@ -8,36 +10,50 @@ yum-config-manager --enable ol7_addons
 yum-config-manager --enable ol7_preview
 yum-config-manager --enable ol7_developer
 
-# Install docker and update everything.
+echo "******************************************************************************"
+echo "Install Docker." `date`
+echo "******************************************************************************"
 yum install docker-engine btrfs-progs btrfs-progs-devel git -y
 #yum update -y
 
-# Prepare the drive for the docker images.
+echo "******************************************************************************"
+echo "Prepare the drive for the docker images." `date`
+echo "******************************************************************************"
 ls /dev/sd*
 echo -e "n\np\n\n\n\nw" | fdisk /dev/sdb
 ls /dev/sd*
 docker-storage-config -s btrfs -d /dev/sdb1
 
-# Enable experimental features.
+echo "******************************************************************************"
+echo "Enable experimental features." `date`
+echo "******************************************************************************"
 sed -i -e "s|OPTIONS='--selinux-enabled'|OPTIONS='--selinux-enabled --experimental=true'|g" /etc/sysconfig/docker
 
-# Enable Docker
+echo "******************************************************************************"
+echo "Enable Docker." `date`
+echo "******************************************************************************"
 systemctl enable docker.service
 systemctl start docker.service
 systemctl status docker.service
 
-# Create non-root docker user.
+echo "******************************************************************************"
+echo "Create non-root docker user." `date`
+echo "******************************************************************************"
 useradd docker_user
 echo "docker_user  ALL=(ALL)  NOPASSWD: /usr/bin/docker" >> /etc/sudoers
 echo "alias docker=\"sudo /usr/bin/docker\"" >> /home/docker_user/.bash_profile
 
-# Configure docker-compose.
+echo "******************************************************************************"
+echo "Configure docker-compose." `date`
+echo "******************************************************************************"
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 echo "docker_user  ALL=(ALL)  NOPASSWD: /usr/local/bin/docker-compose" >> /etc/sudoers
 echo "alias docker-compose=\"sudo /usr/local/bin/docker-compose\"" >> /home/docker_user/.bash_profile
 
-# Copy the "docker_user_setup.sh" config file from the vagrant directory and run it.
+echo "******************************************************************************"
+echo "Copy setup files to the local disks." `date`
+echo "******************************************************************************"
 cp /vagrant/scripts/docker_user_setup.sh /home/docker_user/
 chown docker_user:docker_user /home/docker_user/docker_user_setup.sh
 chmod +x /home/docker_user/docker_user_setup.sh
