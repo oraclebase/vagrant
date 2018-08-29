e2ddb5d077a1fb57db50ff5340a38ac53a4b128a
Tim Hall <tim@oracle-base.com>
2018-08-29 07:19:05 +0100

    Add better listener config.

--- ol7_em13c/scripts/oracle_create_database.sh
+++ ol7_em13c/scripts/oracle_create_database.sh
@@ -1,21 +1,53 @@
 # Create a listener.ora file if it doesn't already exist.
 if [ ! -f ${ORACLE_HOME}/network/admin/listener.ora ]; then
-echo "LISTENER =
-(DESCRIPTION_LIST =
-  (DESCRIPTION =
-    (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1))
-    (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+
+  cat > ${ORACLE_HOME}/network/admin/listener.ora <<EOF
+LISTENER = 
+(DESCRIPTION_LIST = 
+  (DESCRIPTION = 
+    (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1)) 
+    (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521)) 
+  ) 
+) 
+USE_SID_AS_SERVICE_listener=on
+INBOUND_CONNECT_TIMEOUT_LISTENER=400
+EOF
+
+  cat > ${ORACLE_HOME}/network/admin/tnsnames.ora <<EOF
+LISTENER = (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+${ORACLE_SID}= 
+(DESCRIPTION = 
+  (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+  (CONNECT_DATA =
+    (SERVER = DEDICATED)
+    (SERVICE_NAME = ${ORACLE_SID})
   )
 )
-USE_SID_AS_SERVICE_listener=on
-" > ${ORACLE_HOME}/network/admin/listener.ora
+${PDB_NAME}= 
+(DESCRIPTION = 
+  (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+  (CONNECT_DATA =
+    (SERVER = DEDICATED)
+    (SERVICE_NAME = ${PDB_NAME})
+  )
+)
+EOF
+
+  cat > ${ORACLE_HOME}/network/admin/sqlnet.ora <<EOF
+SQLNET.INBOUND_CONNECT_TIMEOUT=400
+EOF
+
 fi
 
 
-# Check if database already exists.
+echo "******************************************************************************"
+echo "Check if database already exists." `date`
+echo "******************************************************************************"
 if [ ! -d ${DATA_DIR}/${ORACLE_SID} ]; then
 
-  # The database files don't exist, so create a new database.
+  echo "******************************************************************************"
+  echo "The database files don't exist, so create a new database." `date`
+  echo "******************************************************************************"
   lsnrctl start
 
   dbca -silent -createDatabase                                                 \
@@ -37,11 +69,13 @@ if [ ! -d ${DATA_DIR}/${ORACLE_SID} ]; then
     -emConfiguration NONE                                                      \
     -ignorePreReqs
 
-  # Set the PDB to auto-start.
+  echo "******************************************************************************"
+  echo "Set the PDB to auto-start." `date`
+  echo "******************************************************************************"
   sqlplus / as sysdba <<EOF
 alter system set db_create_file_dest='${DATA_DIR}';
 alter pluggable database ${PDB_NAME} save state;
-alter system set local_listener='localhost';
+alter system set local_listener='LISTENER';
 
 -- Recommended settings.
 alter system set "_allow_insert_with_update_check"=true scope=both;
@@ -52,7 +86,9 @@ exit;
 EOF
 
 
-  # Flip the auto-start flag.
+  echo "******************************************************************************"
+  echo "Flip the auto-start flag." `date`
+  echo "******************************************************************************"
   cp /etc/oratab /tmp
   sed -i -e "s|${ORACLE_SID}:${ORACLE_HOME}:N|${ORACLE_SID}:${ORACLE_HOME}:Y|g" /tmp/oratab
   cp -f /tmp/oratab /etc/oratab
