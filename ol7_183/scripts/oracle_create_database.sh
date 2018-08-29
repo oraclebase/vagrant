542353131bfe5df4337c9a1ed14871315235a4a6
Tim Hall <tim@oracle-base.com>
2018-08-29 07:18:41 +0100

    Add better listener config.

--- ol7_183/scripts/oracle_create_database.sh
+++ ol7_183/scripts/oracle_create_database.sh
@@ -2,15 +2,43 @@ echo "**************************************************************************
 echo " Create a listener.ora file if it doesn't already exist." `date`
 echo "******************************************************************************"
 if [ ! -f ${ORACLE_HOME}/network/admin/listener.ora ]; then
-echo "LISTENER =
-(DESCRIPTION_LIST =
-  (DESCRIPTION =
-    (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1))
-    (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+  
+  cat > ${ORACLE_HOME}/network/admin/listener.ora <<EOF
+LISTENER = 
+(DESCRIPTION_LIST = 
+  (DESCRIPTION = 
+    (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1)) 
+    (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521)) 
+  ) 
+) 
+USE_SID_AS_SERVICE_listener=on
+INBOUND_CONNECT_TIMEOUT_LISTENER=400
+EOF
+
+  cat > ${ORACLE_HOME}/network/admin/tnsnames.ora <<EOF
+LISTENER = (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+${ORACLE_SID}= 
+(DESCRIPTION = 
+  (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+  (CONNECT_DATA =
+    (SERVER = DEDICATED)
+    (SERVICE_NAME = ${ORACLE_SID})
   )
 )
-USE_SID_AS_SERVICE_listener=on
-" > ${ORACLE_HOME}/network/admin/listener.ora
+${PDB_NAME}= 
+(DESCRIPTION = 
+  (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+  (CONNECT_DATA =
+    (SERVER = DEDICATED)
+    (SERVICE_NAME = ${PDB_NAME})
+  )
+)
+EOF
+
+  cat > ${ORACLE_HOME}/network/admin/sqlnet.ora <<EOF
+SQLNET.INBOUND_CONNECT_TIMEOUT=400
+EOF
+
 fi
 
 
@@ -49,7 +77,7 @@ if [ ! -d ${DATA_DIR}/${ORACLE_SID^^} ]; then
   sqlplus / as sysdba <<EOF
 alter system set db_create_file_dest='${DATA_DIR}';
 alter pluggable database ${PDB_NAME} save state;
-alter system set local_listener='localhost';
+alter system set local_listener='LISTENER';
 exit;
 EOF
 
