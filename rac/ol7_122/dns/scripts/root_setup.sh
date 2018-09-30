a9f4585ac72f9bd3710a0224858a5c61eafc1cdd
Tim Hall <tim@oracle-base.com>
2018-09-30 16:49:57 +0100

    Add 8.8.8.8 nameserver and switch to using HTTPS for Yum repository.

--- rac/ol7_122/dns/scripts/root_setup.sh
+++ rac/ol7_122/dns/scripts/root_setup.sh
@@ -3,11 +3,10 @@
 echo "******************************************************************************"
 echo "Prepare yum with the latest repos." `date`
 echo "******************************************************************************"
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 cd /etc/yum.repos.d
-#rm -f public-yum-ol6.repo
-#wget http://yum.oracle.com/public-yum-ol6.repo
 rm -f public-yum-ol7.repo
-wget http://yum.oracle.com/public-yum-ol7.repo
+wget https://yum.oracle.com/public-yum-ol7.repo
 
 sh /vagrant_scripts/configure_hosts_base.sh
 sh /vagrant_scripts/configure_hosts_scan.sh
