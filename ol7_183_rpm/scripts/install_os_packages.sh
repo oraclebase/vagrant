9a44f51ca1b7832428b96cfa0e306027619149e6
Tim Hall <tim@oracle-base.com>
2018-10-22 05:34:42 -0700

    Remove the preinstall dependencies package, as it is pulled down automatically during the RPM installation.

--- ol7_183_rpm/scripts/install_os_packages.sh
+++ ol7_183_rpm/scripts/install_os_packages.sh
@@ -10,11 +10,6 @@ wget https://yum.oracle.com/public-yum-ol7.repo
 yum install -y yum-utils zip unzip
 
 echo "******************************************************************************"
-echo "Install Oracle prerequisite package." `date`
-echo "******************************************************************************"
-yum install -y oracle-database-preinstall-18c
-
-echo "******************************************************************************"
 echo "Install Oracle RPM." `date`
 echo "******************************************************************************"
 yum -y localinstall /vagrant/software/oracle-database-ee-18c-1.0-1.x86_64.rpm
