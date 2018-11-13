e4f166f91cb968cb84ed3dc6ad9e1900243f2b0e
Tim Hall <tim@oracle-base.com>
2018-11-13 19:04:56 +0000

    Update Java and Tomcat versions.

--- ol7_183/scripts/oracle_user_environment_setup.sh
+++ ol7_183/scripts/oracle_user_environment_setup.sh
@@ -38,8 +38,8 @@ export DATA_DIR=/u02/oradata
 
 
 # ORDS installation settings.
-export JAVA_SOFTWARE="openjdk-11_linux-x64_bin.tar.gz"
-export TOMCAT_SOFTWARE="apache-tomcat-9.0.12.tar.gz"
+export JAVA_SOFTWARE="openjdk-11.0.1_linux-x64_bin.tar.gz"
+export TOMCAT_SOFTWARE="apache-tomcat-9.0.13.tar.gz"
 export ORDS_SOFTWARE="ords-18.3.0.270.1456.zip"
 export APEX_SOFTWARE="apex_18.2_en.zip"
 export SQLCL_SOFTWARE="sqlcl-18.3.0.259.2029.zip"
