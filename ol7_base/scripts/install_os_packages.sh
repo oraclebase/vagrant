c9170384cc5d70bedae35862cb52b7db8d81913b
Tim Hall <tim@oracle-base.com>
2019-01-29 17:49:42 +0000

    First commit.

---
+++ ol7_base/scripts/install_os_packages.sh
@@ -0,0 +1,10 @@
+echo "******************************************************************************"
+echo "Prepare yum with the latest repos." `date`
+echo "******************************************************************************"
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+#cd /etc/yum.repos.d
+#rm -f public-yum-ol7.repo
+#wget https://yum.oracle.com/public-yum-ol7.repo
+
+#yum update -y
