3bf95290ff864896b2f8ef8773dcc3834c35ee3e
Tim Hall <tim@oracle-base.com>
2019-07-20 10:47:46 +0100

    Change comment to reflect move to ol8.

--- ol8_183/scripts/install_os_packages.sh
+++ ol8_183/scripts/install_os_packages.sh
@@ -4,8 +4,8 @@ echo "**************************************************************************
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
 #cd /etc/yum.repos.d
-#rm -f public-yum-ol7.repo
-#wget https://yum.oracle.com/public-yum-ol7.repo
+#rm -f public-yum-ol8.repo
+#wget https://yum.oracle.com/public-yum-ol8.repo
 
 dnf install -y dnf-utils zip unzip
 
