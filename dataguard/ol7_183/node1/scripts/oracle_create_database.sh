077b9b72679c2f114ebce8dc75ffd492af176980
Tim Hall <tim@oracle-base.com>
2019-12-21 12:15:34 +0000

    Changes suggested by Peter Wahl and Richard Evans.
    Add support for DB_DOMAIN.
    Add support for Native Network Encryption.
    Fix standby redo logs.

--- dataguard/ol7_183/node1/scripts/oracle_create_database.sh
+++ dataguard/ol7_183/node1/scripts/oracle_create_database.sh
@@ -5,12 +5,12 @@ echo "Configure network scripts." `date`
 echo "******************************************************************************"
 
 cat > ${ORACLE_HOME}/network/admin/tnsnames.ora <<EOF
-LISTENER = (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE1_HOSTNAME})(PORT = 1521))
+LISTENER = (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE1_FQ_HOSTNAME})(PORT = 1521))
 
-${NODE1_DB_UNIQUE_NAME} =
+${NODE1_DB_UNIQUE_NAME}${DB_DOMAIN_STR} =
   (DESCRIPTION =
     (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE1_HOSTNAME})(PORT = 1521))
+      (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE1_FQ_HOSTNAME})(PORT = 1521))
     )
     (CONNECT_DATA =
       (SERVER = DEDICATED)
@@ -18,10 +18,10 @@ ${NODE1_DB_UNIQUE_NAME} =
     )
   )
 
-${NODE2_DB_UNIQUE_NAME} =
+${NODE2_DB_UNIQUE_NAME}${DB_DOMAIN_STR} =
   (DESCRIPTION =
     (ADDRESS_LIST =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE2_HOSTNAME})(PORT = 1521))
+      (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE2_FQ_HOSTNAME})(PORT = 1521))
     )
     (CONNECT_DATA =
       (SERVER = DEDICATED)
@@ -35,7 +35,7 @@ cat > ${ORACLE_HOME}/network/admin/listener.ora <<EOF
 LISTENER =
   (DESCRIPTION_LIST =
     (DESCRIPTION =
-      (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE1_HOSTNAME})(PORT = 1521))
+      (ADDRESS = (PROTOCOL = TCP)(HOST = ${NODE1_FQ_HOSTNAME})(PORT = 1521))
       (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1521))
     )
   )
@@ -43,9 +43,20 @@ LISTENER =
 SID_LIST_LISTENER =
   (SID_LIST =
     (SID_DESC =
-      (GLOBAL_DBNAME = ${NODE1_DB_UNIQUE_NAME}_DGMGRL)
-      (ORACLE_HOME = ${ORACLE_HOME})
+      (GLOBAL_DBNAME = ${NODE1_DB_UNIQUE_NAME})
       (SID_NAME = ${ORACLE_SID})
+      (ORACLE_HOME = ${ORACLE_HOME})
+    )
+    (SID_DESC =
+      (GLOBAL_DBNAME = ${NODE1_DB_UNIQUE_NAME}${DB_DOMAIN_STR})
+      (SID_NAME = ${ORACLE_SID})
+      (ORACLE_HOME = ${ORACLE_HOME})
+    )
+    (SID_DESC =
+      (SID_NAME = ${ORACLE_SID})
+      (GLOBAL_DBNAME = ${NODE1_DB_UNIQUE_NAME}_DGMGRL${DB_DOMAIN_STR})
+      (ORACLE_HOME = ${ORACLE_HOME})
+      (ENVS="TNS_ADMIN=${ORACLE_HOME}/network/admin")
     )
   )
 
@@ -58,17 +69,45 @@ cat > ${ORACLE_HOME}/network/admin/sqlnet.ora <<EOF
 SQLNET.INBOUND_CONNECT_TIMEOUT=400
 EOF
 
+# Adding the Native Network Encryption was suggested by Peter Wahl and Richard Evans.
+# I've made it optional.
+if [ "${NATIVE_NETWORK_ENCRYPTION}" = "true" ]; then
+  cat >> ${ORACLE_HOME}/network/admin/sqlnet.ora <<EOF
+SQLNET.ENCRYPTION_SERVER=REQUIRED
+SQLNET.ENCRYPTION_TYPES_SERVER=(AES256)
+
+SQLNET.ENCRYPTION_CLIENT=REQUIRED
+SQLNET.ENCRYPTION_TYPES_CLIENT=(AES256)
+
+SQLNET.CRYPTO_CHECKSUM_SERVER=REQUIRED
+SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER = (SHA256)
+
+SQLNET.CRYPTO_CHECKSUM_CLIENT=REQUIRED
+SQLNET.CRYPTO_CHECKSUM_TYPES_CLIENT = (SHA256)
+EOF
+fi
+
+if [ "${DB_DOMAIN}" != "" ]; then
+  cat >> ${ORACLE_HOME}/network/admin/sqlnet.ora <<EOF
+NAMES.DEFAULT_DOMAIN=${DB_DOMAIN}
+EOF
+fi
+
 echo "******************************************************************************"
 echo "Start listener." `date`
 echo "******************************************************************************"
+lsnrctl stop
 lsnrctl start
+lsnrctl status
 
 echo "******************************************************************************"
 echo "Create database." `date`
 echo "******************************************************************************"
 dbca -silent -createDatabase                                                 \
   -templateName General_Purpose.dbc                                          \
-  -gdbname ${ORACLE_SID} -sid ${ORACLE_SID} -responseFile NO_VALUE           \
+  -sid ${ORACLE_SID}                                                         \
+  -responseFile NO_VALUE                                                     \
+  -gdbname ${NODE1_DB_UNIQUE_NAME}${DB_DOMAIN_STR}                           \
   -characterSet AL32UTF8                                                     \
   -sysPassword ${SYS_PASSWORD}                                               \
   -systemPassword ${SYS_PASSWORD}                                            \
@@ -92,7 +131,7 @@ sqlplus / as sysdba <<EOF
 ALTER SYSTEM SET db_create_file_dest='${DATA_DIR}';
 ALTER SYSTEM SET db_create_online_log_dest_1='${DATA_DIR}';
 ALTER PLUGGABLE DATABASE ${PDB_NAME} SAVE STATE;
-ALTER SYSTEM SET local_listener='LISTENER';
+ALTER SYSTEM RESET local_listener;
 ALTER SYSTEM SET db_recovery_file_dest_size=20G;
 ALTER SYSTEM SET db_recovery_file_dest='/u01/app/oracle';
 exit;
@@ -114,12 +153,12 @@ ALTER DATABASE FORCE LOGGING;
 ALTER SYSTEM SWITCH LOGFILE;
 
 -- Add standby logs.
-ALTER DATABASE ADD STANDBY LOGFILE SIZE 50M;
-ALTER DATABASE ADD STANDBY LOGFILE SIZE 50M;
-ALTER DATABASE ADD STANDBY LOGFILE SIZE 50M;
-ALTER DATABASE ADD STANDBY LOGFILE SIZE 50M;
+ALTER DATABASE ADD STANDBY LOGFILE THREAD 1 GROUP 10 SIZE 50M;
+ALTER DATABASE ADD STANDBY LOGFILE THREAD 1 GROUP 11 SIZE 50M;
+ALTER DATABASE ADD STANDBY LOGFILE THREAD 1 GROUP 12 SIZE 50M;
+ALTER DATABASE ADD STANDBY LOGFILE THREAD 1 GROUP 13 SIZE 50M;
 -- If you don't want to use OMF specify a path like this.
---ALTER DATABASE ADD STANDBY LOGFILE ('${DATA_DIR}/${ORACLE_SID^^}/standby_redo01.log') SIZE 50M;
+--ALTER DATABASE ADD STANDBY LOGFILE ('${DATA_DIR}/${ORACLE_SID^^}/standby_redo01.log') THREAD 1 GROUP 10 SIZE 50M;
 
 -- Enable flashback database.
 ALTER DATABASE FLASHBACK ON;
