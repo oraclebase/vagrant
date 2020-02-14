e685a2358e8febe0f2c078b8ad6a8a9468421ae3
Tim Hall <tim@oracle-base.com>
2020-02-14 11:17:19 +0000

    Remove references to old repo files.

--- em/ol7_em13c/scripts/install_os_packages.sh
+++ em/ol7_em13c/scripts/install_os_packages.sh
@@ -3,9 +3,6 @@ echo "Prepare yum with the latest repos." `date`
 echo "******************************************************************************"
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
-cd /etc/yum.repos.d
-rm -f public-yum-ol7.repo
-wget https://yum.oracle.com/public-yum-ol7.repo
 yum -y install yum-utils zip unzip
 yum -y install oracle-database-preinstall-18c
 yum -y install make
