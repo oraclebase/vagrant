b6ed53799932956d00f1ff4523bb680636b83723
Tim Hall <tim@oracle-base.com>
2020-02-17 22:21:34 +0000

    Remove management of old OL7 repo files.

--- rac/ol7_122/shared_scripts/install_os_packages.sh
+++ rac/ol7_122/shared_scripts/install_os_packages.sh
@@ -2,10 +2,9 @@ echo "**************************************************************************
 echo "Prepare yum repos and install base packages." `date`
 echo "******************************************************************************"
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-cd /etc/yum.repos.d
-rm -f public-yum-ol7.repo
-wget https://yum.oracle.com/public-yum-ol7.repo
+
 yum install -y yum-utils
+yum install -y oracle-epel-release-el7
 yum-config-manager --enable ol7_developer_EPEL
 yum install -y sshpass zip unzip
 yum install -y oracle-database-server-12cR2-preinstall
