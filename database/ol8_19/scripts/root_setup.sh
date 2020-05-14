05cfb666559b89988df7148a63f6618229999b1b
Tim Hall <tim@oracle-base.com>
2020-05-14 19:35:36 +0100

    Move the patching down below the root scripts.

--- database/ol8_19/scripts/root_setup.sh
+++ database/ol8_19/scripts/root_setup.sh
@@ -30,15 +30,16 @@ echo "Prepare environment and install the software." `date`
 echo "******************************************************************************"
 su - oracle -c '/u01/software/oracle_user_environment_setup.sh'
 su - oracle -c '/u01/software/oracle_software_installation.sh'
-# Uncomment the following line if you want to apply patches.
-# You must have downloaded the patches and amended the "oracle_software_patch.sh" script.
-#su - oracle -c '/u01/software/oracle_software_patch.sh'
 
 echo "******************************************************************************"
 echo "Run root scripts." `date`
 echo "******************************************************************************"
 sh ${ORA_INVENTORY}/orainstRoot.sh
 sh ${ORACLE_HOME}/root.sh
+
+# Uncomment the following line if you want to apply patches.
+# You must have downloaded the patches and amended the "oracle_software_patch.sh" script.
+#su - oracle -c '/u01/software/oracle_software_patch.sh'
 
 echo "******************************************************************************"
 echo "Create the database and install the ORDS software." `date`
