853e47d3d265fb8762849ed2dd46ab95185b2158
Tim Hall <tim@oracle-base.com>
2020-10-22 21:04:29 +0100

    Update the patch script to use the October 2020 patches.

--- database/ol8_19/scripts/oracle_software_patch.sh
+++ database/ol8_19/scripts/oracle_software_patch.sh
@@ -7,10 +7,10 @@ echo "**************************************************************************
 # Adjust to suit your patch level.
 export PATH=${ORACLE_HOME}/OPatch:${PATH}
 export OPATCH_FILE="p6880880_190000_Linux-x86-64.zip"
-export PATCH_FILE="p31326362_190000_Linux-x86-64.zip"
-export PATCH_TOP=${SOFTWARE_DIR}/31326362
-export PATCH_PATH1=${PATCH_TOP}/31281355
-export PATCH_PATH2=${PATCH_TOP}/31219897
+export PATCH_FILE="p31720396_190000_Linux-x86-64.zip"
+export PATCH_TOP=${SOFTWARE_DIR}/31720396
+export PATCH_PATH1=${PATCH_TOP}/31668882
+export PATCH_PATH2=${PATCH_TOP}/31771877
 
 echo "******************************************************************************"
 echo "Prepare opatch." `date`
