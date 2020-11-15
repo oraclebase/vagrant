8882503a96c8af26f297dd2b385d9c5878815d2e
Tim Hall <tim@oracle-base.com>
2020-11-15 14:34:27 +0000

    Add build for Oracle 12.1 on Oracle Linux 8 (OL8).

---
+++ database/ol8_121/README.md
@@ -0,0 +1,47 @@
+# Oracle 12.1 on Oracle Linux 8
+
+A simple Vagrant build for Oracle Database 12.1 on Oracle Linux 8.
+
+## Required Software
+
+* [Vagrant](https://www.vagrantup.com/downloads.html)
+* [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+* [Oracle Database](https://www.oracle.com/technetwork/database/enterprise-edition/downloads/database12c-linux-download-2240591.html)
+* [Oracle REST Data Services (ORDS)](https://www.oracle.com/technetwork/developer-tools/rest-data-services/downloads/)
+* [Oracle SQLcl](https://www.oracle.com/tools/downloads/sqlcl-downloads.html)
+* [Oracle Application Express (APEX)](https://www.oracle.com/tools/downloads/apex-downloads.html)
+* [OpenJDK 11](https://adoptopenjdk.net/releases.html?variant=openjdk11&jvmVariant=hotspot#x64_linux)
+* [Tomcat 9](https://tomcat.apache.org/download-90.cgi)
+
+Place the Oracle database software in the "software" directory before calling the `vagrant up` command.
+
+Directory contents when software is included.
+
+```
+$ tree
+.
++--- README.md
++--- scripts
+|   +--- dbora.service
+|   +--- install_os_packages.sh
+|   +--- oracle_create_database.sh
+|   +--- oracle_service_setup.sh
+|   +--- oracle_software_installation.sh
+|   +--- oracle_user_environment_setup.sh
+|   +--- ords_software_installation.sh
+|   +--- prepare_disks.sh
+|   +--- root_setup.sh
+|   +--- server.xml
+|   +--- setup.sh
++--- software
+|   +--- apache-tomcat-9.0.39.tar.gz
+|   +--- apex_20.2_en.zip
+|   +--- linuxamd64_12102_database_1of2.zip
+|   +--- linuxamd64_12102_database_2of2.zip
+|   +--- OpenJDK11U-jdk_x64_linux_hotspot_11.0.9_11.tar.gz
+|   +--- ords-20.3.0.301.1819.zip
+|   +--- put_software_here.txt
+|   +--- sqlcl-20.3.0.274.1916.zip
++--- Vagrantfile
+$
+```
---
+++ database/ol8_121/scripts/oracle_user_environment_setup.sh
@@ -0,0 +1,119 @@
+echo "******************************************************************************"
+echo "Create environment script." `date`
+echo "******************************************************************************"
+cat > /home/oracle/scripts/setEnv.sh <<EOF
+# Regular settings.
+export TMP=/tmp
+export TMPDIR=\$TMP
+
+export ORACLE_HOSTNAME=`hostname`
+export ORACLE_UNQNAME=cdb1
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=\$ORACLE_BASE/product/12.1.0.2/db_1
+export ORACLE_SID=cdb1
+
+export PATH=/usr/sbin:/usr/local/bin:\$PATH
+export PATH=\$ORACLE_HOME/bin:\$PATH
+
+export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
+export CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib
+
+export ORA_INVENTORY=/u01/app/oraInventory
+
+
+# Database installation settings.
+export SOFTWARE_DIR=/u01/software
+export DB_SOFTWARE="linuxamd64_12102_database_*of2.zip"
+export APEX_SOFTWARE="apex_20.2_en.zip"
+export ORACLE_PASSWORD="oracle"
+export SCRIPTS_DIR=/home/oracle/scripts
+
+export ORACLE_SID=cdb1
+export SYS_PASSWORD="SysPassword1"
+export PDB_NAME="pdb1"
+export PDB_PASSWORD="PdbPassword1"
+export APEX_EMAIL="me@example.com"
+export APEX_PASSWORD="ApexPassword1"
+export DATA_DIR=/u02/oradata
+
+export INSTALL_APEX="true"
+export INSTALL_ORDS="true"
+
+
+# ORDS installation settings.
+export JAVA_SOFTWARE="OpenJDK11U-jdk_x64_linux_hotspot_11.0.9_11.tar.gz"
+export TOMCAT_SOFTWARE="apache-tomcat-9.0.39.tar.gz"
+export ORDS_SOFTWARE="ords-20.3.0.301.1819.zip"
+export SQLCL_SOFTWARE="sqlcl-20.3.0.274.1916.zip"
+export SOFTWARE_DIR="/u01/software"
+export KEYSTORE_DIR="/u01/keystore"
+export ORDS_HOME="/u01/ords"
+export ORDS_CONF="/u01/ords/conf"
+export JAVA_HOME="/u01/java"
+export CATALINA_HOME="/u01/tomcat"
+export CATALINA_BASE=\$CATALINA_HOME
+
+export DB_PORT="1521"
+export DB_SERVICE="pdb1"
+export APEX_PUBLIC_USER_PASSWORD="ApexPassword1"
+export APEX_TABLESPACE="APEX"
+export TEMP_TABLESPACE="TEMP"
+export APEX_LISTENER_PASSWORD="ApexPassword1"
+export APEX_REST_PASSWORD="ApexPassword1"
+export PUBLIC_PASSWORD="ApexPassword1"
+export SYS_PASSWORD="SysPassword1"
+export KEYSTORE_PASSWORD="KeystorePassword1"
+EOF
+
+
+echo "******************************************************************************"
+echo "Add it to the .bash_profile." `date`
+echo "******************************************************************************"
+echo ". /home/oracle/scripts/setEnv.sh" >> /home/oracle/.bash_profile
+
+
+echo "******************************************************************************"
+echo "Create start/stop scripts." `date`
+echo "******************************************************************************"
+. /home/oracle/scripts/setEnv.sh
+
+cat > /home/oracle/scripts/start_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbstart \$ORACLE_HOME
+EOF
+
+
+cat > /home/oracle/scripts/stop_all.sh <<EOF
+#!/bin/bash
+. /home/oracle/scripts/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbshut \$ORACLE_HOME
+EOF
+
+# Add Tomcat management if required.
+if [ "${INSTALL_ORDS}" = "true" ]; then
+  
+cat >> /home/oracle/scripts/start_all.sh <<EOF
+
+\$CATALINA_HOME/bin/startup.sh
+EOF
+
+cat >> /home/oracle/scripts/stop_all.sh <<EOF
+
+\$CATALINA_HOME/bin/shutdown.sh
+EOF
+
+fi
+
+chown -R oracle.oinstall ${SCRIPTS_DIR}
+chmod u+x ${SCRIPTS_DIR}/*.sh
