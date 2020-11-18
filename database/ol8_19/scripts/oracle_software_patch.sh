10f12abffde4e4d43a66164719cbc328e8f83ce7
Tim Hall <tim@oracle-base.com>
2020-11-18 08:29:01 +0000

    Use central config. Access software and scripts from /vagrant directory, rather than copying into VM.

--- database/ol8_19/scripts/oracle_software_patch.sh
+++ database/ol8_19/scripts/oracle_software_patch.sh
@@ -1,30 +1,26 @@
+. /vagrant/config/install.env
+
 # This patch script should only be used for a clean installation.
 # It doesn't patch existing databases.
 echo "******************************************************************************"
 echo "Patch Oracle Software." `date`
 echo "******************************************************************************"
  
-# Adjust to suit your patch level.
 export PATH=${ORACLE_HOME}/OPatch:${PATH}
-export OPATCH_FILE="p6880880_190000_Linux-x86-64.zip"
-export PATCH_FILE="p31720396_190000_Linux-x86-64.zip"
-export PATCH_TOP=${SOFTWARE_DIR}/31720396
-export PATCH_PATH1=${PATCH_TOP}/31668882
-export PATCH_PATH2=${PATCH_TOP}/31771877
 
 echo "******************************************************************************"
 echo "Prepare opatch." `date`
 echo "******************************************************************************"
 
 cd ${ORACLE_HOME}
-unzip -oq ${SOFTWARE_DIR}/${OPATCH_FILE}
+unzip -oq /vagrant/software/${OPATCH_FILE}
 
 echo "******************************************************************************"
 echo "Unzip software." `date`
 echo "******************************************************************************"
 
 cd ${SOFTWARE_DIR}
-unzip -oq ${PATCH_FILE}
+unzip -oq /vagrant/software/${PATCH_FILE}
 
 echo "******************************************************************************"
 echo "Apply patches." `date`
