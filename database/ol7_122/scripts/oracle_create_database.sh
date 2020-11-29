df5655dd1894b709b909c21d42f479cd3c470a21
Tim Hall <tim@oracle-base.com>
2020-11-29 18:06:19 +0000

    Double quote the APEX passwords.

--- database/ol7_122/scripts/oracle_create_database.sh
+++ database/ol7_122/scripts/oracle_create_database.sh
@@ -122,10 +122,10 @@ BEGIN
     COMMIT;
 END;
 /
-@apex_rest_config.sql ${APEX_PASSWORD} ${APEX_PASSWORD}
+@apex_rest_config.sql "${APEX_PASSWORD}" "${APEX_PASSWORD}"
 --@apex_epg_config.sql ${SOFTWARE_DIR}
-alter user APEX_PUBLIC_USER identified by ${APEX_PASSWORD} account unlock;
-alter user APEX_REST_PUBLIC_USER identified by ${APEX_PASSWORD} account unlock;
+alter user APEX_PUBLIC_USER identified by "${APEX_PASSWORD}" account unlock;
+alter user APEX_REST_PUBLIC_USER identified by "${APEX_PASSWORD}" account unlock;
 exit;
 EOF
 
