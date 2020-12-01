74608519f852b79d0a73a5f6aaf14fc25120b5f2
Tim Hall <tim@oracle-base.com>
2020-12-01 11:35:01 +0000

    Remove old repository management. Install oracle-epel-release-el7 package. Remove python-pip install. Add python-pip3 and libselinux-python3 packages.

--- awx/scripts/install_os_packages.sh
+++ awx/scripts/install_os_packages.sh
@@ -3,21 +3,19 @@ echo "Prepare yum with the latest repos." `date`
 echo "******************************************************************************"
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
-cd /etc/yum.repos.d
-rm -f public-yum-ol7.repo
-wget http://yum.oracle.com/public-yum-ol7.repo
 yum install -y yum-utils zip unzip
 yum-config-manager --enable ol7_optional_latest
 yum-config-manager --enable ol7_addons
 yum-config-manager --enable ol7_preview
 yum-config-manager --enable ol7_developer
+yum install -y oracle-epel-release-el7
 yum-config-manager --enable ol7_developer_EPEL
 
 echo "******************************************************************************"
 echo "Install Docker and Ansible." `date`
 echo "******************************************************************************"
 yum install -y docker-engine btrfs-progs btrfs-progs-devel
-yum install -y ansible python-pip python-devel python3-devel
+yum install -y ansible python3-pip python3-devel libselinux-python3
 
 # AWX requirement.
 yum install -y git gcc
