10a68f2343d387d19484e8ed2b22f7f6f6ecd460
Tim Hall <tim@oracle-base.com>
2021-02-27 12:14:57 +0000

    Uncomment the reposync of ol8_baseos_latest.

--- miscellaneous/ol8_yum/scripts/repo_sync.sh
+++ miscellaneous/ol8_yum/scripts/repo_sync.sh
@@ -9,10 +9,10 @@ echo "**************************************************************************
 echo "Sync repositories." `date`
 echo "******************************************************************************"
 /usr/bin/reposync --newest-only --repoid=ol8_UEKR6 -p /u01/repo/OracleLinux >> $LOG_FILE 2>&1
-#/usr/bin/reposync --newest-only --repoid=ol8_baseos_latest -p /u01/repo/OracleLinux >> $LOG_FILE 2>&1
+/usr/bin/reposync --newest-only --repoid=ol8_baseos_latest -p /u01/repo/OracleLinux >> $LOG_FILE 2>&1
 
 echo "******************************************************************************"
 echo "Recreate repositories." `date`
 echo "******************************************************************************"
 /usr/bin/createrepo /u01/repo/OracleLinux/ol8_UEKR6/getPackage/
-#/usr/bin/createrepo /u01/repo/OracleLinux/ol8_baseos_latest/getPackage/
+/usr/bin/createrepo /u01/repo/OracleLinux/ol8_baseos_latest/getPackage/
