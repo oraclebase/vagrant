b57a0855f07da3912e2b0a06bbb819402be50531
Tim Hall <tim@oracle-base.com>
2021-06-14 20:16:34 +0100

    Add "--format docker" flag for builds.

--- containers/ol8/podman/scripts/builds.sh
+++ containers/ol8/podman/scripts/builds.sh
@@ -26,7 +26,7 @@ cp /vagrant/software/ords-21.1.1.116.2032.zip .
 cp /vagrant/software/sqlcl-21.1.0.104.1544.zip .
 cp /vagrant/software/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz .
 cd /u01/dockerfiles/ords/ol7_ords
-podman build --no-cache -t ol7_ords:latest .
+podman build --format docker --no-cache -t ol7_ords:latest .
 
 # Copy ORDS software and do build (OL8).
 cd /u01/dockerfiles/ords/ol8_ords/software
@@ -36,14 +36,14 @@ cp /vagrant/software/ords-21.1.1.116.2032.zip .
 cp /vagrant/software/sqlcl-21.1.0.104.1544.zip .
 cp /vagrant/software/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz .
 cd /u01/dockerfiles/ords/ol8_ords
-podman build --no-cache -t ol8_ords:latest .
+podman build --format docker --no-cache -t ol8_ords:latest .
 
 # Copy database software and do build (OL7).
 cd /u01/dockerfiles/database/ol7_19/software
 cp /vagrant/software/LINUX.X64_193000_db_home.zip .
 cp /vagrant/software/apex_21.1_en.zip .
 cd /u01/dockerfiles/database/ol7_19
-podman build --no-cache -t ol7_19:latest .
+podman build --format docker --no-cache -t ol7_19:latest .
 
 # Copy database software and do build (OL8).
 cd /u01/dockerfiles/database/ol8_19/software
@@ -52,35 +52,35 @@ cp /vagrant/software/apex_21.1_en.zip .
 cp /vagrant/software/p6880880_190000_Linux-x86-64.zip .
 cp /vagrant/software/p32578972_190000_Linux-x86-64.zip .
 cd /u01/dockerfiles/database/ol8_19
-podman build --no-cache -t ol8_19:latest .
+podman build --format docker --no-cache -t ol8_19:latest .
 
 # Copy database software and do build (OL7).
 cd /u01/dockerfiles/database/ol7_183/software
 cp /vagrant/software/LINUX.X64_180000_db_home.zip .
 cp /vagrant/software/apex_21.1_en.zip .
 cd /u01/dockerfiles/database/ol7_183
-#podman build --no-cache -t ol7_183:latest .
+#podman build --format docker --no-cache -t ol7_183:latest .
 
 # Copy database software and do build (OL8).
 cd /u01/dockerfiles/database/ol8_183/software
 cp /vagrant/software/LINUX.X64_180000_db_home.zip .
 cp /vagrant/software/apex_21.1_en.zip .
 cd /u01/dockerfiles/database/ol8_183
-#podman build --no-cache -t ol8_183:latest .
+#podman build --format docker --no-cache -t ol8_183:latest .
 
 # Copy database software and don't do build.
 cd /u01/dockerfiles/database/ol7_122/software
 cp /vagrant/software/linuxx64_12201_database.zip .
 cp /vagrant/software/apex_21.1_en.zip .
 cd /u01/dockerfiles/database/ol7_122
-#podman build --no-cache -t ol7_122:latest .
+#podman build --format docker --no-cache -t ol7_122:latest .
 
 # Copy database software and don't do build.
 cd /u01/dockerfiles/database/ol7_121/software
 cp /vagrant/software/linuxamd64_12102_database_*of2.zip .
 cp /vagrant/software/apex_21.1_en.zip .
 cd /u01/dockerfiles/database/ol7_121
-#podman build --no-cache -t ol7_121:latest .
+#podman build --format docker --no-cache -t ol7_121:latest .
 
 # Setup file system to allow container_user to interact
 # with podman host volumes.
