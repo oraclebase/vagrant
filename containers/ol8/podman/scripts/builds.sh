1e399dac5c4f400108f38cadb2d438b1348c8017
Tim Hall <tim@oracle-base.com>
2021-07-13 23:11:26 +0100

    Update to ORDS 21.2, SQLcl 21.2 and Tomcat 9.0.50.

--- containers/ol8/podman/scripts/builds.sh
+++ containers/ol8/podman/scripts/builds.sh
@@ -5,7 +5,7 @@ echo "export JAVA_HOME=/home/container_user/java/latest" >> ~/.bash_profile
 echo "alias sql=\"/home/container_user/sqlcl/bin/sql\"" >> ~/.bash_profile
 
 cd ~
-unzip -oq /vagrant/software/sqlcl-21.1.0.104.1544.zip
+unzip -oq /vagrant/software/sqlcl-21.2.0.174.2245.zip
 mkdir ~/java
 cd ~/java
 tar -xf /vagrant/software/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz
@@ -21,9 +21,9 @@ podman pull oraclelinux:8
 # Copy ORDS software and do build (OL7).
 cd /u01/dockerfiles/ords/ol7_ords/software
 cp /vagrant/software/apex_21.1_en.zip .
-cp /vagrant/software/apache-tomcat-9.0.45.tar.gz .
-cp /vagrant/software/ords-21.1.1.116.2032.zip .
-cp /vagrant/software/sqlcl-21.1.0.104.1544.zip .
+cp /vagrant/software/apache-tomcat-9.0.50.tar.gz .
+cp /vagrant/software/ords-21.2.0.174.1826.zip .
+cp /vagrant/software/sqlcl-21.2.0.174.2245.zip .
 cp /vagrant/software/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz .
 cd /u01/dockerfiles/ords/ol7_ords
 podman build --format docker --no-cache -t ol7_ords:latest .
@@ -31,9 +31,9 @@ podman build --format docker --no-cache -t ol7_ords:latest .
 # Copy ORDS software and do build (OL8).
 cd /u01/dockerfiles/ords/ol8_ords/software
 cp /vagrant/software/apex_21.1_en.zip .
-cp /vagrant/software/apache-tomcat-9.0.45.tar.gz .
-cp /vagrant/software/ords-21.1.1.116.2032.zip .
-cp /vagrant/software/sqlcl-21.1.0.104.1544.zip .
+cp /vagrant/software/apache-tomcat-9.0.50.tar.gz .
+cp /vagrant/software/ords-21.2.0.174.1826.zip .
+cp /vagrant/software/sqlcl-21.2.0.174.2245.zip .
 cp /vagrant/software/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz .
 cd /u01/dockerfiles/ords/ol8_ords
 podman build --format docker --no-cache -t ol8_ords:latest .
