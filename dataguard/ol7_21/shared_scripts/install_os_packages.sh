662c1ea151bd12df6c39803bbf5217465f02e66c
Tim Hall <tim@oracle-base.com>
2021-08-15 13:27:40 +0100

    Add Oracle 21c Data Guard on OL7.

---
+++ dataguard/ol7_21/shared_scripts/install_os_packages.sh
@@ -0,0 +1,11 @@
+echo "******************************************************************************"
+echo "Prepare yum repos and install base packages." `date`
+echo "******************************************************************************"
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+yum install -y yum-utils
+yum install -y oracle-epel-release-el7
+yum-config-manager --enable ol7_developer_EPEL
+yum install -y zip unzip # sshpass 
+yum install -y oracle-database-preinstall-19c
+#yum -y update
