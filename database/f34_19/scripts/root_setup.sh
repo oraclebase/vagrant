bc463692d9bbd57e72a0109c95ffb54e568db65a
Tim Hall <tim@oracle-base.com>
2021-10-18 18:02:52 +0100

    Add 19c installation on Fedora 34 (f34_19).

---
+++ database/f34_19/scripts/root_setup.sh
@@ -0,0 +1,27 @@
+. /vagrant/config/install.env
+
+sh /vagrant/scripts/prepare_disks.sh
+
+sh /vagrant/scripts/install_os_packages.sh
+
+mkdir -p ${SCRIPTS_DIR}
+mkdir -p ${SOFTWARE_DIR}
+mkdir -p ${DATA_DIR}
+chown -R oracle.oinstall ${SCRIPTS_DIR} /u01 /u02
+
+echo "******************************************************************************"
+echo "Prepare environment and install the software." `date`
+echo "******************************************************************************" 
+su - oracle -c 'sh /vagrant/scripts/oracle_user_environment_setup.sh'
+su - oracle -c 'sh /vagrant/scripts/oracle_software_installation.sh'
+
+echo "******************************************************************************"
+echo "Run root scripts." `date`
+echo "******************************************************************************" 
+sh ${ORA_INVENTORY}/orainstRoot.sh
+sh ${ORACLE_HOME}/root.sh
+
+echo "******************************************************************************"
+echo "Create the database." `date`
+echo "******************************************************************************" 
+su - oracle -c 'sh /vagrant/scripts/oracle_create_database.sh'
