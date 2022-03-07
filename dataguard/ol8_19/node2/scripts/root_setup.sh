e466d0a74163521e1502ff6739b45c060dca9105
Daniel Overby Hansen <daniel@dohdatabase.com>
2022-03-07 12:47:10 +0100

    My first changes

--- dataguard/ol8_19/node2/scripts/root_setup.sh
+++ dataguard/ol8_19/node2/scripts/root_setup.sh
@@ -29,8 +29,6 @@ echo "**************************************************************************
 sh ${ORA_INVENTORY}/orainstRoot.sh
 sh ${ORACLE_HOME}/root.sh
 
-if [ "${PATCH_DB}" = "true" ]; then
-  su - oracle -c 'sh /vagrant_scripts/oracle_software_patch.sh'
-fi
+su - oracle -c 'sh /vagrant_scripts/oracle_software_patch.sh'
 
 su - oracle -c 'sh /vagrant/scripts/oracle_create_database.sh'
