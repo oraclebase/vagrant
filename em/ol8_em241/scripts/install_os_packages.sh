6d9623110d8285abb2ef8d6fcab4ebe0fff71f93
Tim Hall <tim@oracle-base.com>
2024-12-20 19:20:49 +0000

    Make package selection match those from docs now it is released.

--- em/ol8_em241/scripts/install_os_packages.sh
+++ em/ol8_em241/scripts/install_os_packages.sh
@@ -5,25 +5,15 @@ echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
 dnf install -y oracle-database-preinstall-19c
 
+dnf install -y make
 dnf install -y binutils
 dnf install -y gcc
-dnf install -y gcc-c++
-dnf install -y glibc
-dnf install -y glibc-devel
-dnf install -y glibc-devel.i686
 dnf install -y libaio
-dnf install -y libaio-devel
-dnf install -y libgcc
 dnf install -y libstdc++
-dnf install -y libstdc++-devel
-dnf install -y libnsl
 dnf install -y sysstat
-dnf install -y motif
-dnf install -y motif-devel
-dnf install -y redhat-lsb
-dnf install -y redhat-lsb-core
-dnf install -y openssl
-dnf install -y openssh-server
-dnf install -y make
+dnf install -y glibc-devel
+dnf install -y glibc-common
+dnf install -y libXtst
+dnf install -y libnsl
 
 #dnf -y update
