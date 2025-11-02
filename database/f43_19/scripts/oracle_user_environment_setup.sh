701b45fd8ef810ff2951dceb488085bde7bbef5e
Tim Hall <tim@oracle-base.com>
2025-11-02 15:08:47 +0000

    Add Fedora 43 builds.

---
+++ database/f43_19/scripts/oracle_user_environment_setup.sh
@@ -0,0 +1,62 @@
+. /vagrant/config/install.env
+
+echo "******************************************************************************"
+echo "Create environment script." `date`
+echo "******************************************************************************"
+mkdir -p /u01/tmp
+
+cat > ${SCRIPTS_DIR}/setEnv.sh <<EOF
+# Regular settings.
+export TMP=/u01/tmp
+export TMPDIR=\$TMP
+
+export ORACLE_HOSTNAME=`hostname`
+export ORACLE_UNQNAME=${ORACLE_UNQNAME}
+export ORACLE_BASE=${ORACLE_BASE}
+export ORACLE_HOME=\$ORACLE_BASE/${ORACLE_HOME_EXT}
+export ORACLE_SID=${ORACLE_SID}
+
+export PATH=/usr/sbin:/usr/local/bin:\$PATH
+export PATH=\$ORACLE_HOME/bin:\$PATH
+
+export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib
+export CLASSPATH=\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib
+
+export ORA_INVENTORY=${ORA_INVENTORY}
+EOF
+
+
+# Add it to the .bash_profile.
+echo ". ${SCRIPTS_DIR}/setEnv.sh" >> /home/oracle/.bash_profile
+
+
+echo "******************************************************************************"
+echo "Create start/stop scripts." `date`
+echo "******************************************************************************"
+. ${SCRIPTS_DIR}/setEnv.sh
+
+cat > ${SCRIPTS_DIR}/start_all.sh <<EOF
+#!/bin/bash
+. ${SCRIPTS_DIR}/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbstart \$ORACLE_HOME
+EOF
+
+
+cat > ${SCRIPTS_DIR}/stop_all.sh <<EOF
+#!/bin/bash
+. ${SCRIPTS_DIR}/setEnv.sh
+
+export ORAENV_ASK=NO
+. oraenv
+export ORAENV_ASK=YES
+
+dbshut \$ORACLE_HOME
+EOF
+
+chown -R oracle:oinstall ${SCRIPTS_DIR}
+chmod u+x ${SCRIPTS_DIR}/*.sh
---
+++ packer/f43/README.md
@@ -0,0 +1,25 @@
+# Packer Fedora 43
+
+This box is based on the initial Fedora 43 release.
+
+Get the Packer software.
+
+* [https://www.packer.io/downloads](https://www.packer.io/downloads)
+
+To build the VirtualBox box add the Packer software to your path, switch to the root of the Packer build directory and run the Packer command. Amend your paths as needed.
+
+```
+set PATH=%USERPROFILE%\u01\software\hashicorp\packer_1.12.0_windows_amd64;%PATH%
+cd \git\oraclebase\vagrant\packer\f43
+
+packer build -only virtualbox-iso f43.json
+```
+
+This will create a box called "f43-x86_64-virtualbox.box" in the "build" directory.
+
+You can then upload the resulting box to Vagrant Cloud, or just use it locally by adding it to you local box list.
+
+```
+vagrant box remove oraclebase/fedora-43
+vagrant box add build/f43-x86_64-virtualbox.box --name oraclebase/fedora-43
+```
