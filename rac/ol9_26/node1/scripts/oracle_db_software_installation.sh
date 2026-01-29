008372c433c5b5b95c74d5f679b22c6ddf5f1965
Tim Hall <tim@oracle-base.com>
2026-01-29 15:36:43 +0000

    Use new parameters for the DB software installation.

--- rac/ol9_26/node1/scripts/oracle_db_software_installation.sh
+++ rac/ol9_26/node1/scripts/oracle_db_software_installation.sh
@@ -11,23 +11,18 @@ echo "Do database software-only installation." `date`
 echo "******************************************************************************"
 ${ORACLE_HOME}/runInstaller -ignorePrereq -waitforcompletion -silent \
         -responseFile ${ORACLE_HOME}/install/response/db_install.rsp \
-        oracle.install.option=INSTALL_DB_SWONLY \
+        installOption=INSTALL_DB_SWONLY \
         ORACLE_HOSTNAME=${ORACLE_HOSTNAME} \
         UNIX_GROUP_NAME=oinstall \
         INVENTORY_LOCATION=${ORA_INVENTORY} \
         SELECTED_LANGUAGES=${ORA_LANGUAGES} \
         ORACLE_HOME=${ORACLE_HOME} \
         ORACLE_BASE=${ORACLE_BASE} \
-        oracle.install.db.InstallEdition=EE \
-        oracle.install.db.OSDBA_GROUP=dba \
-        oracle.install.db.OSBACKUPDBA_GROUP=dba \
-        oracle.install.db.OSDGDBA_GROUP=dba \
-        oracle.install.db.OSKMDBA_GROUP=dba \
-        oracle.install.db.OSRACDBA_GROUP=dba \
-        oracle.install.db.CLUSTER_NODES=${NODE1_HOSTNAME},${NODE2_HOSTNAME} \
-        oracle.install.db.isRACOneInstall=false \
-        oracle.install.db.rac.serverpoolCardinality=0 \
-        oracle.install.db.config.starterdb.type=GENERAL_PURPOSE \
-        oracle.install.db.ConfigureAsContainerDB=false \
-        SECURITY_UPDATES_VIA_MYORACLESUPPORT=false \
-        DECLINE_SECURITY_UPDATES=true
+        installEdition=EE \
+        OSDBA=dba \
+        OSBACKUPDBA=dba \
+        OSDGDBA=dba \
+        OSKMDBA=dba \
+        OSRACDBA=dba \
+        clusterNodes=${NODE1_HOSTNAME},${NODE2_HOSTNAME} \
+        dbType=GENERAL_PURPOSE
